ad933806a8cccc14fa572df32cc3b42b668e6766
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-02-22 11:06:57 +0800

    Update app.js
    R901

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ async function reverseGeocode() {
     document.getElementById("compassLokasi").innerText = lokasiFinal;
     document.getElementById("compassKoordinat").innerText =
       `${userLat.toFixed(6)}, ${userLng.toFixed(6)} - ${elevation.toFixed(2)} mdpl`;
-  } catch(e){ document.getElementById("namaLokasi").innerText = "📍 Silakan refresh halaman.."; }
+  } catch(e){ document.getElementById("namaLokasi").innerText = "📍 Nama lokasi tidak ditemukan.."; }
 }
 
 /* ===============================
